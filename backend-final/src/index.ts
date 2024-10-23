6e2e77231f51362660ab69a6887a919f230ee9c2
SukhvirKooner <sukhvirkooner05@gmail.com>
2024-10-23 22:10:50 +0530

    authentication backend

--- backend-final/src/index.ts
+++ backend-final/src/index.ts
@@ -21,15 +21,59 @@ async function createUser(firstName:string,lastName:string,email:string,password
     return res;
 
 }
+async function getUser(email:string) {
+
+    const res = await prisma.user.findFirst({
+        where:{
+            email
+        }
+    })
+    return res;
+    
+}
 
 app.get('/', ( req: Request,res: Response) => {
   res.send('Hello from TypeScript Express!');
 });
 
 app.post("/signup", async (req: Request, res: Response) => {
+    let check = await getUser(req.body.email);
+    if(check){
+        res.status(400).json({success:false,errors:"user already exists"});
+    }
     const user =await createUser(req.body.firstName, req.body.lastName, req.body.email, req.body.password);
-    res.json(user);
+    const data = {
+        user:{
+            id:user.id
+        }
+    }
+    const token = jwt.sign(data,"TodoList");
+    res.json({success:true,token}); 
+
+
 });
+
+app.get("/login",async(req:Request,res:Response)=>{
+    const user = await getUser(req.body.email);
+    if(user){
+        const comparePass = req.body.password == user.password;
+        if(comparePass){
+            console.log('successfully login')
+            const data = {
+                user:{
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data,"TodoList");
+            res.json({success:true,token});
+        }
+        else{
+            res.json({success:false,errors:"wrong password"})
+        }
+    }else{
+        res.json({success:false,errors:"wrong email"});
+    }
+})
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
