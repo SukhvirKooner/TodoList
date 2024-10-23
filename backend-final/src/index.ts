0049e459afa6b682c852caba4c05ba6fb755808b
SukhvirKooner <sukhvirkooner05@gmail.com>
2024-10-23 18:09:45 +0530

    userCreated

--- backend-final/src/index.ts
+++ backend-final/src/index.ts
@@ -1,15 +1,33 @@
 import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client'; 
 
 const app = express();
-const port = 3000; 
+const port = 4000; 
 const jwt = require('jsonwebtoken');
 app.use(express.json());
 
+const prisma = new PrismaClient;
 
+async function createUser(firstName:string,lastName:string,email:string,password:string) {
+    const res = await prisma.user.create({
+        data:{
+            firstName,
+            lastName,
+            email,
+            password
+        }
+    })
+    console.log(res);
+    
+}
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from TypeScript Express!');
 });
+
+app.post("/signup",(res:Response,req:Request)=>{
+
+})
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
